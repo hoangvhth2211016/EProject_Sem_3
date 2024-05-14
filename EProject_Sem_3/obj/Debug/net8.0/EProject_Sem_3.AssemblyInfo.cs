63d126f56cfbfe907fbd6e15e471754badc5e86a
hoangvhth2211016 <hoangvhth2211016@fpt.edu.vn>
2024-05-14 21:22:10 +0700

    save local

--- EProject_Sem_3/obj/Debug/net8.0/EProject_Sem_3.AssemblyInfo.cs
+++ EProject_Sem_3/obj/Debug/net8.0/EProject_Sem_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c82f0ae7a60cf3e84a296aea91db0af4051d83a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869815ffa0b9b1f40099f32d102d0676e15c70c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
