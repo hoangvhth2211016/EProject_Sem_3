6a705c32e28240ca2afcb986315d683537f0f9c6
quanganh <quanganhnguyen100196@gmail.com>
2024-05-15 09:18:12 +0700

    a

--- EProject_Sem_3/obj/Debug/net8.0/EProject_Sem_3.AssemblyInfo.cs
+++ EProject_Sem_3/obj/Debug/net8.0/EProject_Sem_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f65fb128933c9364300175b6686d21db5b6baa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0adfeb61a588e150706787e4d748220372028e9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
