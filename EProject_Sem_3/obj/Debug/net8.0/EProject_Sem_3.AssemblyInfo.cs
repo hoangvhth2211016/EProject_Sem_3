82b543472e9a072e2d5ba9e7f37de669196cc243
quanganh <quanganhnguyen100196@gmail.com>
2024-05-21 11:30:34 +0700

    fix totalAmount

--- EProject_Sem_3/obj/Debug/net8.0/EProject_Sem_3.AssemblyInfo.cs
+++ EProject_Sem_3/obj/Debug/net8.0/EProject_Sem_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d268737436e5d64013032fd124463189034829")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3f19a4a01b49944524f8a67b69cfd57d47e9ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EProject_Sem_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
